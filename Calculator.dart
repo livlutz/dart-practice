3f3b0dff6112c2860ba2bd02536679f5d9994614
Livia <livialutz385@gmail.com>
2023-09-10 23:04:22 -0300

    Calculator upgrade with functions

--- Calculator.dart
+++ Calculator.dart
@@ -2,34 +2,45 @@ import 'dart:convert';
 import 'dart:io';
 void main(List<String> args) {
 
-  var line;
+  print("Welcome to the calculator");
 
-  print("1st number: ");
-  line = stdin.readLineSync(encoding: utf8);
-  var number1 = double.parse(line ?? "0");
+  var number1 = readDouble("1st number: ");
 
-  print("2nd number: ");
-  line = stdin.readLineSync(encoding: utf8);
-  var number2 = double.parse(line ?? "0");
+  var number2 = readDouble("2nd number: ");
 
-  print("Operation: ");
-  line = stdin.readLineSync(encoding: utf8);
-  var op = line ?? "";
+  var op = readConsole("Operation (+,-,*,/): ");
 
+  calculator(op, number1, number2);
+}
+
+double readDouble(String text){
+  var numero = double.tryParse(readConsole(text));
+  
+  if(numero == null){
+    print("Invalid number set to 0");
+    return 0.0;
+  }
+
+  else{
+    return numero;
+  }
+}
+
+void calculator(String op, double number1, double number2){
   double result = 0;
 
   switch(op){
     case "+":
-      result = number1 + number2;
+      result = sum(number1, number2);
       break;
     case "-":
-      result = number1 - number2;
+      result = sub(number1, number2);
       break;
     case "*":
-      result = number1 * number2;
+      result = mult(number1, number2);
       break;
     case "/":
-      result = number1 / number2;
+      result = div(number1, number2);
       break;
 
     default:
@@ -38,4 +49,32 @@ void main(List<String> args) {
 
   print("Operation: $op");
   print("Result: $result");
+}
+
+double sum(double number1, double number2){
+  return number1 + number2;
+}
+
+double sub(double number1, double number2){
+  return number1 - number2;
+}
+
+double mult(double number1, double number2){
+  return number1 * number2;
+}
+
+double div(double number1, double number2){
+  if(number2 == 0){
+    print("Zero division error");
+    exit(0);
+  }
+  else{
+    return number1 / number2;
+  }
+}
+
+String readConsole(String s){
+  print(s);
+  var line = stdin.readLineSync(encoding: utf8);
+  return line ?? "";
 }
