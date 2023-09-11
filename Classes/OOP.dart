045586ddb0fa038f137c078158f44de9ba764536
Livia <livialutz385@gmail.com>
2023-09-11 01:35:51 -0300

    Some notes in OOP

---
+++ Classes/OOP.dart
@@ -0,0 +1,7 @@
+
+import 'Person.dart';
+
+void main(List <String> arguments){
+  Person p1 = new Person("John", "123 Main St.");
+  print(p1);
+}
---
+++ Classes/Person.dart
@@ -0,0 +1,36 @@
+
+class Person{
+
+  //_ means private
+  String _name = "";
+  String _address = "";
+
+  void SetName(String name){
+    _name = name;
+  }
+
+  String GetName(){
+    return _name.toUpperCase();
+  }
+
+  void SetAddress(String address){
+    _address = address;
+  }
+
+  String GetAddress(){
+    return _address;
+  }
+
+  //constructor
+  Person(String name, String address){
+    _name = name;
+    _address = address;
+  }
+
+  //override toString method
+  @override
+  String toString(){
+    return {"Name": _name, "Address": _address}.toString();
+  }
+ 
+}
